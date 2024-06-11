e8d6024959ecd1337826d4d4141b034509a51bda
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-06-11 21:02:34 +0530

    Apply NOT NULL and create database pgAdmin

--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
-CREATE TABLE book_notes (
+CREATE TABLE books (
     id SERIAL PRIMARY KEY,
-    title TEXT,
-    author TEXT,
-    isbn TEXT,
-    rating INTEGER,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    isbn TEXT NOT NULL,
+    rating INTEGER NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     review TEXT
 );
 
 CREATE TABLE notes (
     id SERIAL PRIMARY KEY,
-    book_id INTEGER REFERENCES book_notes(id),
-    notes TEXT
+    book_id INTEGER REFERENCES books(id),
+    note TEXT NOT NULL
 );
