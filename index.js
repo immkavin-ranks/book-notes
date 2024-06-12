60cf5d9f02a4e7a6e5cb2528b9c605fb8bf7120e
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-06-12 13:23:58 +0530

    Implement sort by rating and recency

--- index.js
+++ index.js
@@ -19,9 +19,31 @@ app.use(express.static("public"));
 let books = [];
 
 app.get("/", async (req, res) => {
-  const response = await db.query("SELECT * FROM books");
-  books = response.rows;
-  res.render("index.ejs", { books: books });
+  const sort = req.query.sort || "default";
+
+  let orderByClause;
+  switch (sort) {
+    case "high-rated":
+      orderByClause = "rating DESC";
+      break;
+    case "most-recent":
+      orderByClause = "updated_at DESC";
+      break;
+    default:
+      orderByClause = "id ASC";
+      break;
+  }
+
+  try {
+    const response = await db.query(
+      `SELECT * FROM books ORDER BY ${orderByClause}`
+    );
+    books = response.rows;
+    res.render("index.ejs", { books: books });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
 });
 
 app.get("/book/:notes_link", async (req, res) => {
@@ -29,7 +51,6 @@ app.get("/book/:notes_link", async (req, res) => {
   const book = books.find(
     (book) => book.title.replace(/[^a-zA-Z0-9]+/g, "") === notes_link
   );
-  // const book_notes = notes.filter((note) => note.book_id === book.id);
   const response = await db.query("SELECT * FROM notes WHERE book_id = $1", [
     book.id,
   ]);
@@ -39,6 +60,14 @@ app.get("/book/:notes_link", async (req, res) => {
     notes: notes,
   });
 });
+
+app.post("/book", async (req, res) => {});
+app.put("/book", async (req, res) => {});
+app.delete("/book", async (req, res) => {});
+
+app.post("/note", async (req, res) => {});
+app.put("/note", async (req, res) => {});
+app.delete("/note", async (req, res) => {});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
